5603f7fda7012a1c7cdf90b5cc106a494f9ce875
Florina B <florabiletsiou@gmail.com>
2024-11-14 00:26:14 +0000

    stylistic changes

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import './HomePage.css';
 
-function Home(props) {
+function Home() {
     return (
-        <div className="mt-2">
-            <h1>Welcome to Oinognostis</h1>
-            <p>
-                Discover the rich world of wines! From deep reds to crisp whites, wines offer a unique taste experience that can enhance any meal or occasion.
-                Whether you're a connoisseur or just beginning your journey, there's a bottle out there for everyone. Explore different varieties, learn about
-                the winemaking process, and find your perfect pour.
-            </p>
-            <p>
-                Join us as we celebrate the art of winemaking and the joy it brings to our lives. Cheers to new discoveries and memorable moments!
-            </p>
+        <div className="home-container">
+            <div className="home-content">
+                <h1>Welcome to Oinognostis</h1>
+                <p>
+                    Discover the rich world of wines! From deep reds to crisp whites, wines offer a unique taste experience that can enhance any meal or occasion.
+                    Whether you're a connoisseur or just beginning your journey, there's a bottle out there for everyone. Explore different varieties, learn about
+                    the winemaking process, and find your perfect pour.
+                </p>
+                <p>
+                    Join us as we celebrate the art of winemaking and the joy it brings to our lives. Cheers to new discoveries and memorable moments!
+                </p>
+            </div>
         </div>
     );
 }
